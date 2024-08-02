27f7cb622142ca7197cc48bb73960802e66a97fc
Bidhan Pradhan <pradhan.bidhan89@gmail.com>
2024-08-03 03:02:37 +0545

    feat: beautified

--- main.go
+++ main.go
@@ -26,11 +26,10 @@ func main() {
 		kubeconfigPath = os.Getenv("HOME") + "/.kube/config"
 	}
 
-       excludedNamespaces := strings.Split(os.Getenv("EXCLUDE_K8S_NS"), ",")
-        if len(excludedNamespaces) == 1 && excludedNamespaces[0] == "" {
-                excludedNamespaces = []string{"kube-system", "default"}
-        }
-
+	excludedNamespaces := strings.Split(os.Getenv("EXCLUDE_K8S_NS"), ",")
+	if len(excludedNamespaces) == 1 && excludedNamespaces[0] == "" {
+		excludedNamespaces = []string{"kube-system", "default"}
+	}
 
 	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
 	if err != nil {
@@ -46,8 +45,7 @@ func main() {
 
 	ctx := context.Background()
 
-
-	filteredNamespaces,_ := GetFilteredNameSpaces(ctx, clientset,excludedNamespaces)
+	filteredNamespaces, _ := GetFilteredNameSpaces(ctx, clientset, excludedNamespaces)
 	fmt.Printf("found %d namespaces from fn\n", len(filteredNamespaces))
 
 	for _, ns := range filteredNamespaces {
@@ -57,52 +55,49 @@ func main() {
 		restartMatchingStatefulSets(ctx, clientset, ns.Name, filterName)
 		restartMatchingDaemonSets(ctx, clientset, ns.Name, filterName)
 
-
 	}
 	fmt.Printf("gracefull restart process completed for pods resource containing keyword %s\n", filterName)
 }
 
-func GetFilteredNameSpaces(ctx context.Context, clientset *kubernetes.Clientset,excludedNamespaces []string) ([]corev1.Namespace,error)  {
+func GetFilteredNameSpaces(ctx context.Context, clientset *kubernetes.Clientset, excludedNamespaces []string) ([]corev1.Namespace, error) {
 
 	namespaces, err := clientset.CoreV1().Namespaces().List(ctx, metav1.ListOptions{})
-        if err != nil {
-                fmt.Printf("error listing namespaces :: %v\n", err)
-                return nil, err
-        }
+	if err != nil {
+		fmt.Printf("error listing namespaces :: %v\n", err)
+		return nil, err
+	}
 
-        var filteredNamespaces []corev1.Namespace
-        for _, ns := range namespaces.Items {
-                if !contains(excludedNamespaces, ns.Name) {
-                        filteredNamespaces = append(filteredNamespaces, ns)
-                }
-        }
+	var filteredNamespaces []corev1.Namespace
+	for _, ns := range namespaces.Items {
+		if !contains(excludedNamespaces, ns.Name) {
+			filteredNamespaces = append(filteredNamespaces, ns)
+		}
+	}
 
-        fmt.Printf("found %d namespaces\n", len(filteredNamespaces))
-	return filteredNamespaces,nil
+	fmt.Printf("found %d namespaces\n", len(filteredNamespaces))
+	return filteredNamespaces, nil
 }
 
+func restartMatchingDeployments(ctx context.Context, clientset *kubernetes.Clientset, namespace, filterName string) error {
+	deployments, err := clientset.AppsV1().Deployments(namespace).List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return fmt.Errorf("error listing deployments: %w", err)
+	}
 
- func restartMatchingDeployments(ctx context.Context, clientset *kubernetes.Clientset, namespace, filterName string) error {
-  deployments, err := clientset.AppsV1().Deployments(namespace).List(ctx, metav1.ListOptions{})
-  if err != nil {
-    return fmt.Errorf("error listing deployments: %w", err)
-  }
+	for _, deployment := range deployments.Items {
+		fmt.Printf("Scanning Deployment %s\n", deployment.Name)
+		if strings.Contains(strings.ToLower(deployment.Name), strings.ToLower(filterName)) {
+			_, err := clientset.AppsV1().Deployments(namespace).
+				Patch(ctx, deployment.Name, types.StrategicMergePatchType, []byte(GetPatchUpdateAnnotationSpec()), metav1.PatchOptions{})
+			if err != nil {
+				return fmt.Errorf("error restarting deployment %s: %w", deployment.Name, err)
+			}
+			fmt.Printf("deployment restarted successfully for %s\n", deployment.Name)
+		}
+	}
 
-  for _, deployment := range deployments.Items {
-     fmt.Printf("Scanning Deployment %s\n",deployment.Name)
-    if strings.Contains(strings.ToLower(deployment.Name), strings.ToLower(filterName)) {
-      _, err := clientset.AppsV1().Deployments(namespace).
-        Patch(ctx, deployment.Name, types.StrategicMergePatchType, []byte(GetPatchUpdateAnnotationSpec()), metav1.PatchOptions{})
-      if err != nil {
-        return fmt.Errorf("error restarting deployment %s: %w", deployment.Name, err)
-      }
-      fmt.Printf("deployment restarted successfully for %s\n", deployment.Name)
-    }
-  }
-
-  return nil
+	return nil
 }
-
 
 func restartMatchingStatefulSets(ctx context.Context, clientset *kubernetes.Clientset, namespace, filterName string) error {
 	statefulsets, err := clientset.AppsV1().StatefulSets(namespace).List(ctx, metav1.ListOptions{})
@@ -111,7 +106,7 @@ func restartMatchingStatefulSets(ctx context.Context, clientset *kubernetes.Clie
 	}
 
 	for _, statefulset := range statefulsets.Items {
-	 	fmt.Printf("Scanning Statefulset %s\n",statefulset.Name)
+		fmt.Printf("Scanning Statefulset %s\n", statefulset.Name)
 		if strings.Contains(strings.ToLower(statefulset.Name), strings.ToLower(filterName)) {
 			_, err := clientset.AppsV1().StatefulSets(namespace).
 				Patch(ctx, statefulset.Name, types.StrategicMergePatchType, []byte(GetPatchUpdateAnnotationSpec()), metav1.PatchOptions{})
@@ -125,33 +120,31 @@ func restartMatchingStatefulSets(ctx context.Context, clientset *kubernetes.Clie
 	return nil
 }
 
-
 func restartMatchingDaemonSets(ctx context.Context, clientset *kubernetes.Clientset, namespace string, filterName string) error {
-    daemonsets, err := clientset.AppsV1().DaemonSets(namespace).List(ctx, metav1.ListOptions{})
-    if err != nil {
-        return fmt.Errorf("error listing daemonsets: %v", err)
-    }
+	daemonsets, err := clientset.AppsV1().DaemonSets(namespace).List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return fmt.Errorf("error listing daemonsets: %v", err)
+	}
 
-    for _, daemonset := range daemonsets.Items {
-	fmt.Printf("Scanning daemonset %s\n",daemonset.Name)
-        if strings.Contains(strings.ToLower(daemonset.Name), filterName) {
-            _, err := clientset.AppsV1().DaemonSets(namespace).Patch(
-                ctx,
-                daemonset.Name,
-                types.StrategicMergePatchType,
-                []byte(GetPatchUpdateAnnotationSpec()),
-                metav1.PatchOptions{},
-            )
-            if err != nil {
-                return fmt.Errorf("error restarting daemonset %s: %v", daemonset.Name, err)
-            }
-            fmt.Printf("daemonset restarted successfully for %s\n", daemonset.Name)
-        }
-    }
+	for _, daemonset := range daemonsets.Items {
+		fmt.Printf("Scanning daemonset %s\n", daemonset.Name)
+		if strings.Contains(strings.ToLower(daemonset.Name), filterName) {
+			_, err := clientset.AppsV1().DaemonSets(namespace).Patch(
+				ctx,
+				daemonset.Name,
+				types.StrategicMergePatchType,
+				[]byte(GetPatchUpdateAnnotationSpec()),
+				metav1.PatchOptions{},
+			)
+			if err != nil {
+				return fmt.Errorf("error restarting daemonset %s: %v", daemonset.Name, err)
+			}
+			fmt.Printf("daemonset restarted successfully for %s\n", daemonset.Name)
+		}
+	}
 
-    return nil
+	return nil
 }
-
 
 func GetPatchUpdateAnnotationSpec() string {
 	return fmt.Sprintf(`{"spec": {"template": {"metadata": {"annotations": {"kubectl.kubernetes.io/restartedAt": "%s"}}}}}`, time.Now().Format(time.RFC3339))
